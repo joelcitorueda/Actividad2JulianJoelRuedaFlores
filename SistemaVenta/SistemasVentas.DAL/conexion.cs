0b21d2899fa49906f0841e5bb3bde5316cc9895c
joelcitorueda <ruedafloresjulian722@gmail.com>
2024-03-28 10:15:53 -0400

    Actividad2 Julian Rueda

---
+++ SistemaVenta/SistemasVentas.DAL/conexion.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SistemasVentas.DAL
+{
+    public class conexion
+    {
+
+        public static string CONECTAR
+        {
+            get { return @"Data Source=DESKTOP-EROEUF5; Initial Catalog=TIENDITABD; Integrated Security=True; TrustServerCertificate=true;"; }
+            //get { return ConfigurationManager.ConnectionStrings["cadena"].ToString(); }
+        }
+        public static DataSet EjecutarDataSet(string consulta)
+        {
+            string p = conexion.CONECTAR;
+            SqlConnection conectar = new SqlConnection(conexion.CONECTAR);
+            conectar.Open();
+            SqlCommand cmd = new SqlCommand(consulta, conectar);
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "TABLA");
+            return ds;
+        }
+
+        public static void Ejecutar(string consulta)
+        {
+            SqlConnection conectar = new SqlConnection(conexion.CONECTAR);
+            conectar.Open();
+            SqlCommand cmd = new SqlCommand(consulta, conectar);
+            cmd.CommandTimeout = 5000;
+            cmd.ExecuteNonQuery();
+        }
+
+        public static int EjecutarEscalar(string consulta)
+        {
+            SqlConnection conectar = new SqlConnection(conexion.CONECTAR);
+            conectar.Open();
+
+            SqlCommand cmd = new SqlCommand(consulta, conectar);
+            cmd.CommandTimeout = 5000;
+            int dev = Convert.ToInt32(cmd.ExecuteScalar());
+            return dev;
+        }
+        public static DataTable EjecutarDataTabla(string consulta, string tabla)
+        {
+            string p = conexion.CONECTAR;
+            SqlConnection conectar = new SqlConnection(conexion.CONECTAR);
+            SqlCommand cmd = new SqlCommand(consulta, conectar);
+            cmd.CommandTimeout = 5000;
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataTable dt = new DataTable(tabla);
+            da.Fill(dt);
+            return dt;
+        }
+		public static bool ValidarCredenciales(string NOMBREUSER, string CONTRASEÑA)
+		{
+			// Consulta SQL para verificar si las credenciales son válidas
+			string consulta = "SELECT COUNT(*) FROM USUARIO WHERE NOMBREUSER = @NOMBREUSER AND CONTRASEÑA = @CONTRASEÑA";
+
+			using (SqlConnection conectar = new SqlConnection(CONECTAR))
+			{
+				using (SqlCommand cmd = new SqlCommand(consulta, conectar))
+				{
+					// Agregar parámetros para evitar inyección SQL
+					cmd.Parameters.AddWithValue("@NOMBREUSER", NOMBREUSER);
+					cmd.Parameters.AddWithValue("@CONTRASEÑA", CONTRASEÑA);
+
+					conectar.Open();
+					int count = (int)cmd.ExecuteScalar();
+
+					// Si count es mayor que cero, significa que las credenciales son válidas
+					return count > 0;
+				}
+			}
+		}
+
+	}
+}
---
+++ SistemaVenta/SistemasVentas.VISTA/LoginIniciarSecion/LoginIniciarSecion.cs
@@ -0,0 +1,43 @@
+﻿using SistemasVentas.DAL;
+using SistemasVentas.VISTA.ClienteVistas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemasVentas.VISTA.LoginIniciarSecion
+{
+	public partial class LoginIniciarSecion : Form
+	{
+		public LoginIniciarSecion()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string nombreUsuario = txtNombreUsuario.Text;
+			string contraseña = txtContraseña.Text;
+			if (ValidarCredenciales(nombreUsuario, contraseña))
+			{
+				MessageBox.Show("Se inicio secion correctamente\nVienvenido a Tiendita Golosal!");
+				ClienteListarVista formulario = new ClienteListarVista();
+				formulario.Show();
+			}
+			else
+			{
+			
+				MessageBox.Show("Nombre de usuario o contraseña incorrectos.");
+			}
+		}
+		private bool ValidarCredenciales(string nombreUsuario, string contrasena)
+		{
+			return conexion.ValidarCredenciales(nombreUsuario, contrasena);
+		}
+	}
+}
